7817a9c9a3f1d34818c6010e3b41880c1af3d7a6
jetpackpony <jetpackpony@gmail.com>
2019-08-10 15:06:40 +0300

    Add timestamp to the page

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-
+const moment = require("moment");
 
 app.get('/*', (req, res) => {
-  res.send(`Much updates. Hello, world! You are requesting '${req.originalUrl}'\n`);
+  res.send(`
+  Much updates. Hello, world! You are requesting '${req.originalUrl}'\n
+  Mongo Host: ${process.env["MONGO_HOST"]}
+  Date here: ${moment().toString()}
+  `);
 });
 
 const PORT = 3000;
