9d5a9f94d65a4418615178ae5504870db333181a
jetpackpony <jetpackpony@gmail.com>
2019-08-14 16:55:45 +0300

    Added basic apollo-server example

--- src/index.js
+++ src/index.js
@@ -1,14 +1,52 @@
-const express = require("express");
-const app = express();
-const moment = require("moment");
+const { ApolloServer, gql } = require('apollo-server');
 
-app.get('/*', (req, res) => {
-  res.send(`
-  Much updates. Hello, world! You are requesting '${req.originalUrl}'\n
-  Mongo Host: ${process.env["MONGO_HOST"]}
-  Date here: ${moment().toString()}
-  `);
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  # Comments in GraphQL are defined with the hash (#) symbol.
+
+  # This "Book" type can be used in other type declarations.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type is the root of all GraphQL queries.
+  # (A "Mutation" type will be covered later on.)
+  type Query {
+    books: [Book]
+  }
+`;
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
-
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
