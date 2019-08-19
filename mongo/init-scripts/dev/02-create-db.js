bb3fbda216ac49e0288ec5d5a94c3943ab3eb790
jetpackpony <jetpackpony@gmail.com>
2019-08-19 13:41:21 +0300

    Change the schema of initial data

--- mongo/init-scripts/dev/02-create-db.js
+++ mongo/init-scripts/dev/02-create-db.js
@@ -1,6 +1,6 @@
 db.foodItems.insertMany([
   {
-    _id: 1,
+    _id: "1",
     title: "Cinnamon Rolls",
     calories: 670,
     protein: 12,
@@ -8,7 +8,7 @@ db.foodItems.insertMany([
     carbs: 132
   },
   {
-    _id: 2,
+    _id: "2",
     title: "Yoghurt",
     calories: 270,
     protein: 54,
@@ -19,19 +19,19 @@ db.foodItems.insertMany([
 
 db.records.insertMany([
   {
-    "foodItem": 1,
+    "foodItemID": "1",
     "weight": 155,
     "eatenAt": (new Date("28 Jul 2019 15:44:32")).toISOString(),
     "createdAt": (new Date("28 Jul 2019 15:44:32")).toISOString(),
   },
   {
-    "foodItem": 1,
+    "foodItemID": "1",
     "weight": 33,
     "eatenAt": (new Date("28 Jul 2019 15:44:32")).toISOString(),
     "createdAt": (new Date("28 Jul 2019 15:44:32")).toISOString(),
   },
   {
-    "foodItem": 2,
+    "foodItemID": "2",
     "weight": 111,
     "eatenAt": (new Date("28 Jul 2019 15:44:32")).toISOString(),
     "createdAt": (new Date("28 Jul 2019 15:44:32")).toISOString(),
