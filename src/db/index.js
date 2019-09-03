b82a2d4adbb6f906c0a21474e2ea61424c5d35f6
jetpackpony <jetpackpony@gmail.com>
2019-09-03 18:46:32 +0300

    Make sure that the default app user exists on app startup

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,56 @@
 const MongoClient = require('mongodb').MongoClient;
 const {
   MONGO_APP_USERNAME, MONGO_APP_PASSWORD,
-  MONGO_HOST, MONGO_PORT, MONGO_DB_NAME
+  MONGO_HOST, MONGO_PORT, MONGO_DB_NAME,
+  APP_USER_NAME, APP_USER_PASSWORD
 } = process.env;
+const { encodePassword } = require("../authHelpers");
 
 const url = `mongodb://${MONGO_APP_USERNAME}:${MONGO_APP_PASSWORD}` 
             + `@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}`;
+
+const createOrUpdateDefaultUser = async (db) => {
+  const user = await db.collection("users").findOne({
+    userName: APP_USER_NAME
+  });
+  if (!user) {
+    console.log("Couldn't find default user. Creating...");
+    const newUser = await db.collection("users").insertOne({
+      userName: APP_USER_NAME,
+      passHash: encodePassword(APP_USER_PASSWORD)
+    });
+    if (!newUser) {
+      throw new Error("Failed to create a default user!");
+    }
+    return newUser;
+  }
+
+  if (user.passHash !== encodePassword(APP_USER_PASSWORD)) {
+    console.log("Password for default user didn't match. Updating...");
+    const newUser = await db.collection("users")
+      .updateOne({
+        _id: user._id,
+      }, {
+          $set: {
+            passHash: encodePassword(APP_USER_PASSWORD)
+          }
+        }
+      );
+    if (!newUser) {
+      throw new Error("Failed to update a default user!");
+    }
+    return newUser;
+  }
+  console.log("Default user exists. All good.");
+  return user;
+};
 
 const initDB = async () => {
   const client = new MongoClient(url, { useNewUrlParser: true });
   try {
     await client.connect();
     const db = client.db(MONGO_DB_NAME);
+    await createOrUpdateDefaultUser(db);
     return {
       db,
       client
@@ -21,7 +60,7 @@ const initDB = async () => {
     console.log(err.stack);
     exit(1);
   }
-}
+};
 
 module.exports = {
   initDB
